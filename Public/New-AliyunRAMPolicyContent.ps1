b59ef447bc1a60a54f78fe2bb682c98aa03d53ca
Zheng Ji Er <ji-er.zheng@ubisoft.com>
2021-11-23 23:09:25 +0800

    remove some public functions

--- Public/New-AliyunRAMPolicyContent.ps1
+++
@@ -1,9 +0,0 @@
-function New-AliyunRAMPolicyContent {
-    Param (
-        $OSSName
-    )
-
-    $PolicyDocument = (Get-Content "$($MyInvocation.MyCommand.Module.ModuleBase)\policy_template.json") -replace '{OSS_NAME}',$OSSName
-
-    return $PolicyDocument
-}
